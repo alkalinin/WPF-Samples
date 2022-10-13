ef82b44524291a56d6db10119902cc5ac62faa4b
Alexander Kalinin <alexander.kalinin@eba-med.com>
2022-10-13 17:29:50 +0200

    Example of the WPF application

---
+++ HelloWorld/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HelloWorld.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int _clicksCount = 0;
+
+    [RelayCommand]
+    private void OnClick() => ClicksCount++;
+}
---
+++ HelloWorld/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Wpf.Ui.Controls;
+
+namespace HelloWorld;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : UiWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Wpf.Ui.Appearance.Accent.ApplySystemAccent();
+    }
+}
